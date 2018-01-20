379a7ac518043b0d918201ca87bdfe298358c6f0
jdruk <devjosafa@gmail.com>
2018-01-20 17:52:11 -0500

    fixed bug capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,42 +2,12 @@
 lock "~> 3.10.1"
 
 set :application, "liliart"
-set :repo_url, "https://github.com/jdruk/lilyart.git"
+set :repo_url, "https://github.com/jdruk/liliart.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'v1'
+set :deploy_to, "/home/deploy/liliart"
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/liliart'
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
 set :console_env, :production
 set :console_user, :deploy
-set :passenger_restart_with_sudo, true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,4 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
 server '178.62.22.163', user: 'deploy', roles: %w{app db web}
-
 
 
 # role-based syntax
